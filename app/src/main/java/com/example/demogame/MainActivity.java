1b536d4f893681f53da54747a70a2fb0b71984a5
DuyVoK15 <90261763+DuyVoK15@users.noreply.github.com>
2023-02-09 16:51:24 +0700

    OK 3

--- app/src/main/java/com/example/demogame/MainActivity.java
+++ app/src/main/java/com/example/demogame/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.demogame;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
@@ -17,17 +18,20 @@ import android.widget.Toast;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView txtPoint;
+    private TextView txtPoint, txtRecievePoint;
     private EditText edtCuoc1, edtCuoc2, edtCuoc3;
     private SeekBar player1, player2, player3;
     private CheckBox cbPlayer1, cbPlayer2, cbPlayer3;
     private Button btnStart, btnRestart, btnNapcard;
     private int point;
     private int count;
+    private static final int POINT = 1000;
+    private int recievePoint;
 
     // Mapping
     private void mapping() {
         txtPoint = findViewById(R.id.point);
+        txtRecievePoint = findViewById(R.id.recievePoint);
         edtCuoc1 = findViewById(R.id.edt01);
         edtCuoc2 = findViewById(R.id.edt02);
         edtCuoc3 = findViewById(R.id.edt03);
@@ -47,12 +51,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        point = 200;
+        MediaPlayer music = MediaPlayer.create(MainActivity.this, R.raw.music);
+        music.start();
+
+        recievePoint = 0;
+        point = POINT;
         mapping();
 
-
-
         btnRestart.setVisibility(View.INVISIBLE);
+        txtRecievePoint.setVisibility(View.INVISIBLE);
 
         player1.setEnabled(false);
         player2.setEnabled(false);
@@ -62,10 +69,11 @@ public class MainActivity extends AppCompatActivity {
         edtCuoc2.setVisibility(View.INVISIBLE);
         edtCuoc3.setVisibility(View.INVISIBLE);
 
+        // Show editext when check box is checked
         cbPlayer1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(cbPlayer1.isChecked()){
+                if (cbPlayer1.isChecked()) {
                     edtCuoc1.setVisibility(View.VISIBLE);
                 } else {
                     edtCuoc1.setVisibility(View.GONE);
@@ -77,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
         cbPlayer2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(cbPlayer2.isChecked()){
+                if (cbPlayer2.isChecked()) {
                     edtCuoc2.setVisibility(View.VISIBLE);
                 } else {
                     edtCuoc2.setVisibility(View.GONE);
@@ -89,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
         cbPlayer3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(cbPlayer3.isChecked()){
+                if (cbPlayer3.isChecked()) {
                     edtCuoc3.setVisibility(View.VISIBLE);
                 } else {
                     edtCuoc3.setVisibility(View.GONE);
@@ -100,8 +108,8 @@ public class MainActivity extends AppCompatActivity {
 
 
         count = 0;
-
-        final CountDownTimer countDownTimer = new CountDownTimer(19000, 100) {
+        // CountDownTimer
+        final CountDownTimer countDownTimer = new CountDownTimer(20000, 250) {
             @Override
             public void onTick(long l) {
 
@@ -111,6 +119,7 @@ public class MainActivity extends AppCompatActivity {
 
                 count++;
 
+                // Movation thumb
                 if (count % 2 == 0) {
                     player1.setThumb(ContextCompat.getDrawable(MainActivity.this, R.drawable.icon_bird_green));
                     player2.setThumb(ContextCompat.getDrawable(MainActivity.this, R.drawable.icon_bird_yellow));
@@ -122,47 +131,88 @@ public class MainActivity extends AppCompatActivity {
 
                 }
 
+                // Who win? and get point
                 if (player1.getProgress() >= player1.getMax()) {
                     win(this);
                     Toast.makeText(getApplicationContext(), "The GREEN BIRD win!", Toast.LENGTH_LONG).show();
-                    point+= (cbPlayer1.isChecked()) ? +Integer.parseInt(txtBetAmount1) : -Integer.parseInt(txtBetAmount1);
-                    point+= (cbPlayer2.isChecked()) ? -Integer.parseInt(txtBetAmount2) : +0;
-                    point+= (cbPlayer3.isChecked()) ? -Integer.parseInt(txtBetAmount3) : +0;
 
-                    txtPoint.setText(point+"");
-                    if(isGameOver()){
+                    // Recieve point
+                    recievePoint += (cbPlayer1.isChecked()) ? +Integer.parseInt(txtBetAmount1) : -Integer.parseInt(txtBetAmount1);
+                    recievePoint += (cbPlayer2.isChecked()) ? -Integer.parseInt(txtBetAmount2) : +0;
+                    recievePoint += (cbPlayer3.isChecked()) ? -Integer.parseInt(txtBetAmount3) : +0;
+                    if (recievePoint >= 0) {
+                        txtRecievePoint.setText("+" + recievePoint + "");
+                    } else {
+                        txtRecievePoint.setText(recievePoint + "");
+                    }
+
+                    // Point
+                    point += (cbPlayer1.isChecked()) ? +Integer.parseInt(txtBetAmount1) : -Integer.parseInt(txtBetAmount1);
+                    point += (cbPlayer2.isChecked()) ? -Integer.parseInt(txtBetAmount2) : +0;
+                    point += (cbPlayer3.isChecked()) ? -Integer.parseInt(txtBetAmount3) : +0;
+                    txtPoint.setText(point + "");
+
+                    // Game over
+                    if (isGameOver()) {
                         btnNapcard.setVisibility(View.VISIBLE);
                     }
 
                 }
+
                 if (player2.getProgress() >= player2.getMax()) {
                     win(this);
                     Toast.makeText(getApplicationContext(), "The YELLOW BIRD win!", Toast.LENGTH_LONG).show();
-                    point+= (cbPlayer2.isChecked()) ? +Integer.parseInt(txtBetAmount2) : -Integer.parseInt(txtBetAmount2);
-                    point+= (cbPlayer1.isChecked()) ? -Integer.parseInt(txtBetAmount1) : +0;
-                    point+= (cbPlayer3.isChecked()) ? -Integer.parseInt(txtBetAmount3) : +0;
 
-                    txtPoint.setText(point+"");
-                    if(isGameOver()){
+                    // Recieve point
+                    recievePoint += (cbPlayer2.isChecked()) ? +Integer.parseInt(txtBetAmount2) : -Integer.parseInt(txtBetAmount2);
+                    recievePoint += (cbPlayer1.isChecked()) ? -Integer.parseInt(txtBetAmount1) : +0;
+                    recievePoint += (cbPlayer3.isChecked()) ? -Integer.parseInt(txtBetAmount3) : +0;
+                    if (recievePoint >= 0) {
+                        txtRecievePoint.setText("+" + recievePoint + "");
+                    } else {
+                        txtRecievePoint.setText(recievePoint + "");
+                    }
+
+                    // Point
+                    point += (cbPlayer2.isChecked()) ? +Integer.parseInt(txtBetAmount2) : -Integer.parseInt(txtBetAmount2);
+                    point += (cbPlayer1.isChecked()) ? -Integer.parseInt(txtBetAmount1) : +0;
+                    point += (cbPlayer3.isChecked()) ? -Integer.parseInt(txtBetAmount3) : +0;
+                    txtPoint.setText(point + "");
+
+                    // Game over
+                    if (isGameOver()) {
                         btnNapcard.setVisibility(View.VISIBLE);
                     }
                 }
                 if (player3.getProgress() >= player3.getMax()) {
                     win(this);
                     Toast.makeText(getApplicationContext(), "The RED BIRD win!", Toast.LENGTH_LONG).show();
-                    point+= (cbPlayer3.isChecked()) ? +Integer.parseInt(txtBetAmount3) : -Integer.parseInt(txtBetAmount3);
-                    point+= (cbPlayer2.isChecked()) ? -Integer.parseInt(txtBetAmount2) : +0;
-                    point+= (cbPlayer1.isChecked()) ? -Integer.parseInt(txtBetAmount1) : +0;
 
-                    txtPoint.setText(point+"");
-                    if(isGameOver()){
+                    // Recieve point
+                    recievePoint += (cbPlayer3.isChecked()) ? +Integer.parseInt(txtBetAmount3) : -Integer.parseInt(txtBetAmount3);
+                    recievePoint += (cbPlayer2.isChecked()) ? -Integer.parseInt(txtBetAmount2) : +0;
+                    recievePoint += (cbPlayer1.isChecked()) ? -Integer.parseInt(txtBetAmount1) : +0;
+                    if (recievePoint >= 0) {
+                        txtRecievePoint.setText("+" + recievePoint + "");
+                    } else {
+                        txtRecievePoint.setText(recievePoint + "");
+                    }
+
+                    // Point
+                    point += (cbPlayer3.isChecked()) ? +Integer.parseInt(txtBetAmount3) : -Integer.parseInt(txtBetAmount3);
+                    point += (cbPlayer2.isChecked()) ? -Integer.parseInt(txtBetAmount2) : +0;
+                    point += (cbPlayer1.isChecked()) ? -Integer.parseInt(txtBetAmount1) : +0;
+                    txtPoint.setText(point + "");
+
+                    // Game over
+                    if (isGameOver()) {
                         btnNapcard.setVisibility(View.VISIBLE);
                     }
                 }
-                speedRandom1(randomOfSpeedRandom());
-                speedRandom2(randomOfSpeedRandom());
-                speedRandom3(randomOfSpeedRandom());
-
+                // Set speed Random
+                speedRandom1(randomOfSpeedRandom(randomOfMin(), randomOfMax()));
+                speedRandom2(randomOfSpeedRandom(randomOfMin(), randomOfMax()));
+                speedRandom3(randomOfSpeedRandom(randomOfMin(), randomOfMax()));
             }
 
             @Override
@@ -178,7 +228,13 @@ public class MainActivity extends AppCompatActivity {
 
                 if (betRequire()) {
                     countDownTimer.cancel();
-                    Toast.makeText(MainActivity.this, "Please add less money",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Please add less money", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (isEnoughMoney()) {
+                    countDownTimer.cancel();
+                    Toast.makeText(MainActivity.this, "You are lost money! Please Nạp Nard!", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
@@ -208,6 +264,9 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Click START to start this game", Toast.LENGTH_SHORT).show();
                 btnStart.setVisibility(View.VISIBLE);
                 btnRestart.setVisibility(View.INVISIBLE);
+                txtRecievePoint.setText("0");
+                txtRecievePoint.setVisibility(View.INVISIBLE);
+                recievePoint = 0;
             }
         });
 
@@ -215,9 +274,9 @@ public class MainActivity extends AppCompatActivity {
         btnNapcard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                point += 200;
-                txtPoint.setText(point+"");
-                if(point > 0) {
+                point += POINT;
+                txtPoint.setText(point + "");
+                if (point > 0) {
                     btnNapcard.setVisibility(View.INVISIBLE);
                 }
             }
@@ -226,27 +285,43 @@ public class MainActivity extends AppCompatActivity {
     }
     // End onCreate()
 
-    boolean betRequire(){
+    boolean betRequire() {
 
         int sumBetAmount = 0;
 
-        if(cbPlayer1.isChecked()){
+        if (cbPlayer1.isChecked()) {
             sumBetAmount += Integer.parseInt(edtCuoc1.getText().toString());
         }
-        if (cbPlayer2.isChecked()){
+        if (cbPlayer2.isChecked()) {
             sumBetAmount += Integer.parseInt(edtCuoc2.getText().toString());
         }
-        if (cbPlayer3.isChecked()){
+        if (cbPlayer3.isChecked()) {
             sumBetAmount += Integer.parseInt(edtCuoc3.getText().toString());
         }
-        if (sumBetAmount<=point) return false;
+        if (sumBetAmount <= point) return false;
         return true;
     }
 
-    boolean isGameOver(){
+    boolean isEnoughMoney() {
+        int sumBetAmount = 0;
+        if (cbPlayer1.isChecked()) {
+            sumBetAmount += Integer.parseInt(edtCuoc1.getText().toString());
+        }
+        if (cbPlayer2.isChecked()) {
+            sumBetAmount += Integer.parseInt(edtCuoc2.getText().toString());
+        }
+        if (cbPlayer3.isChecked()) {
+            sumBetAmount += Integer.parseInt(edtCuoc3.getText().toString());
+        }
+        if (sumBetAmount == 0 && point == 0) return true;
+        return false;
+
+    }
+
+    boolean isGameOver() {
         String txtSumPoint = txtPoint.getText().toString();
-        if(Integer.parseInt(txtSumPoint)<=0){
-            Toast.makeText(MainActivity.this,"GAME OVER!! Let's recharge card!", Toast.LENGTH_SHORT).show();
+        if (Integer.parseInt(txtSumPoint) <= 0) {
+            Toast.makeText(MainActivity.this, "GAME OVER!! Let's recharge card!", Toast.LENGTH_SHORT).show();
             return true;
         }
         return false;
@@ -254,26 +329,35 @@ public class MainActivity extends AppCompatActivity {
 
     void speedRandom1(int speed) {
         Random random = new Random();
-        player1.setProgress(player1.getProgress() + random.nextInt(speed));
-
+        player1.setProgress((player1.getProgress() + random.nextInt(speed)));
     }
 
     void speedRandom2(int speed) {
         Random random = new Random();
-        player2.setProgress(player2.getProgress() + random.nextInt(speed));
-
+        player2.setProgress((player2.getProgress() + random.nextInt(speed)));
     }
 
     void speedRandom3(int speed) {
         Random random = new Random();
-        player3.setProgress(player3.getProgress() + random.nextInt(speed));
-
+        player3.setProgress((player3.getProgress() + random.nextInt(speed)));
     }
 
-    int randomOfSpeedRandom(){
-        int max = 20;
-        int min = 7;
-        int rd = (int)Math.random() * (max - min + 1) + min;
+    int randomOfSpeedRandom(int min, int max) {
+        int rd = (int) Math.random() * (max - min + 1) + min;
+        return rd;
+    }
+
+    int randomOfMax() {
+        int max = 150;
+        int min = 50;
+        int rd = (int) Math.random() * (max - min + 1) + min;
+        return rd;
+    }
+
+    int randomOfMin() {
+        int max = 25;
+        int min = 5;
+        int rd = (int) Math.random() * (max - min + 1) + min;
         return rd;
     }
 
@@ -281,6 +365,7 @@ public class MainActivity extends AppCompatActivity {
         enabledCheckBox();
         enabledEditCuoc();
         btnRestart.setVisibility(View.VISIBLE);
+        txtRecievePoint.setVisibility(View.VISIBLE);
         countDownTimer.cancel();
 
     }
@@ -308,7 +393,6 @@ public class MainActivity extends AppCompatActivity {
         edtCuoc2.setFocusable(false);
         edtCuoc3.setFocusable(false);
     }
-
 
 }
 
